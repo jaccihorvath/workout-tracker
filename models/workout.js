d609352d0639434b9b00b1a567c9c3819c26aaf0
Jacci Horvath <jaccinhorvath@gmail.com>
2021-02-22 16:51:06 -0500

    fixing totalDuration function

--- models/workout.js
+++ models/workout.js
@@ -53,7 +53,7 @@ const WorkoutSchema = new Schema({
 // function to get total duration of exercise
 WorkoutSchema.virtual('totalDuration').get(function () {
     return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
+        return total + exercises.duration;
     }, 0);
 });
 
