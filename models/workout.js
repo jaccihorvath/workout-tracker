d4286ec6361a2d27cf5f972a882557b9c4b099cd
Jacci Horvath <jaccinhorvath@gmail.com>
2021-02-22 16:32:07 -0500

    adding workout and index models

---
+++ models/workout.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// new workout schema
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true
+            },
+
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+
+            distance: {
+                type: Number,
+                required: true
+            },
+
+            duration: {
+                type: Number,
+                required: true
+            },
+
+            weight: {
+                type: Number,
+                required: true
+            },
+
+            sets: {
+                type: Number,
+                required: true
+            },
+
+            reps: {
+                type: Number,
+                required: true
+            }
+        }
+    ]
+});
+
+// function to get total duration of exercise
+WorkoutSchema.virtual('totalDuration').get(function () {
+    return this.exercise.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
