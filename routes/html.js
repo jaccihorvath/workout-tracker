94c17994f9f595e830557baa34a7fbc0644a8aba
Jacci Horvath <jaccinhorvath@gmail.com>
2021-02-22 16:46:21 -0500

    fixing html routes

--- routes/html.js
+++ routes/html.js
@@ -1,19 +1,20 @@
-const db = require('../models');
 const path = require('path');
+const router = require('express').Router();
 
 
-module.exports = function (app) {
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-
-    
-    app.get('/exercise', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/exercise.html'));
-    });
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 
-    app.get('/stats', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/stats.html'));
-    });
-};
+router.get('/exercise', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+
+router.get('/stats', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+
+module.exports = router;
